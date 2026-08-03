2b34f5906590c0b05107a6bf4654a3c4fa80cb80
Ethernity12 <235769415+Ethernity12@users.noreply.github.com>
2026-08-04 00:29:15 +0300

    Refactor LLamaCppProvider to enhance chat completion method and add verbose logging

--- app/llm/llamacpp.py
+++ app/llm/llamacpp.py
@@ -5,13 +5,23 @@ from llama_cpp import Llama
 
 class LLamaCppProvider(LLMProvider):
     def __init__(self, model_path, n_gpu_layers, n_ctx, n_threads):
-        self._llm = Llama(model_path=model_path, n_gpu_layers=n_gpu_layers, n_ctx=n_ctx, n_threads=n_threads)
+        self._llm = Llama(model_path=model_path, n_gpu_layers=n_gpu_layers, n_ctx=n_ctx, n_threads=n_threads, verbose=False)
+        print(self._llm.chat_format)
+        print(self._llm.metadata.get("tokenizer.chat_template"))
 
     async def generate(self, prompt):
-        response = self._llm.create_chat_completion(
-            messages=[{"role": "user", "content": prompt}],)
-        
-        return response['choices'][0]['message']['content']
+        response = self._llm.create_chat_completion_openai_v1(
+            max_tokens=2048,
+            temperature=0.6,
+            top_p=0.95,
+            top_k=20,
+            min_p=0,
+            messages=[{"role": "user", "content": prompt}],
+            chat_template_kwargs={
+                "enable_thinking": False
+            }
+        )
+        return response.choices[0].message.content
             
         
     
