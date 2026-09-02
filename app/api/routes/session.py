654c5199ba12d0b4ae7fa74e8b783ab4cf4e341b
Ethernity12 <235769415+Ethernity12@users.noreply.github.com>
2026-09-03 00:24:56 +0300

    Service Managment with CRUD operations and update HTTP routes

--- app/api/routes/session.py
+++ app/api/routes/session.py
@@ -1,5 +1,5 @@
 from uuid import UUID
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, HTTPException, Request, status
 from app.api.schemas.session import SessionRequest, SessionResponse, SessionUpdateRequest
 from app.database.models.sessions import Session
 from app.services.session_service import SessionService
@@ -8,21 +8,29 @@ from app.services.session_service import SessionService
 router = APIRouter()
 
 @router.post("", response_model=SessionResponse)
-async def session_create(request: Request, payload: SessionRequest) -> SessionResponse:
+async def create_session(request: Request, payload: SessionRequest) -> SessionResponse:
     session_service: SessionService = request.app.state.session_service
-    return await session_service.create(title=payload.title, setting=payload.setting)
+    return await session_service.create_session(title=payload.title, setting=payload.setting)
 
-@router.delete("/{session_id}")
-async def session_delete(request: Request, session_id: UUID) -> None:
-    pass
+@router.delete("/{session_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_session(request: Request, session_id: UUID) -> None:
+    session_service: SessionService = request.app.state.session_service
+    await session_service.delete_session(session_id)
 
 @router.patch("/{session_id}", response_model=SessionResponse)
-async def session_update(request: Request, session_id: UUID, payload: SessionUpdateRequest) -> SessionResponse:
-    pass
+async def update_session(request: Request, session_id: UUID, payload: SessionUpdateRequest) -> SessionResponse:
+    session_service: SessionService = request.app.state.session_service
+    session: Session | None = await session_service.update_session(session_id, title=payload.title, setting=payload.setting)
+
+    if session is None:
+        raise HTTPException(status_code=404, detail="Session not found")
+
+    return session
 
 @router.get("/{session_id}", response_model=SessionResponse)
-async def session_get(request: Request, session_id: UUID) -> SessionResponse:
-    session: Session | None = await request.app.state.session_service.get(session_id)
+async def get_session(request: Request, session_id: UUID) -> SessionResponse:
+    session_service: SessionService = request.app.state.session_service
+    session: Session | None = await session_service.get_session(session_id)
 
     if session is None:
         raise HTTPException(status_code=404, detail="Session not found")
--- app/services/session_service.py
+++ app/services/session_service.py
@@ -7,9 +7,22 @@ class SessionService:
     def __init__(self, repository: SessionRepository):
         self._repository = repository
         
-    async def create(self, title: str, setting: str | None) -> Session:
+    async def create_session(self, title: str, setting: str | None) -> Session:
         created_session = await self._repository.create(title=title, setting=setting)
         return created_session
 
-    async def get(self, session_id: UUID) -> Session | None:
+    async def get_session(self, session_id: UUID) -> Session | None:
         return await self._repository.get(session_id)
+    
+    async def update_session(self, session_id: UUID, **values) -> Session | None:
+        session = await self._repository.get(session_id)
+        if session is None:
+            return None
+
+        upd_values = {k: v for k, v in values.items() if v is not None}
+        
+        updated_session = await self._repository.update(session_id, **upd_values)
+        return updated_session
+    
+    async def delete_session(self, session_id: UUID) -> None:
+        await self._repository.delete(session_id)
